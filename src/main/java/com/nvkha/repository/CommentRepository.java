4a4af2aa3125c9c9682116cbde668d4e6ad31a1a
nvkha <nvkha98@gmail.com>
2022-02-10 13:27:30 +0700

    fix some bug

--- src/main/java/com/nvkha/repository/CommentRepository.java
+++ src/main/java/com/nvkha/repository/CommentRepository.java
@@ -2,9 +2,11 @@ package com.nvkha.repository;
 
 import com.nvkha.repository.entity.CommentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
@@ -12,6 +14,8 @@ public interface CommentRepository extends JpaRepository<CommentEntity, Long> {
     @Query(value = "SELECT c FROM CommentEntity c WHERE c.post.id = ?1")
     List<CommentEntity> findAllByPost(Long id);
 
+    @Transactional
+    @Modifying
     @Query(value = "DELETE FROM CommentEntity c WHERE c.post.id = ?1")
     void deleteAllByPost(Long postId);
 }
